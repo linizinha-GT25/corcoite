1cb77cce62888cb212faace11c932569ee23e2b9
Aline Menezes <linizinhamenezes@gmail.com>
2026-08-30 14:43:43 +0000

    feat: adicionar dados simulados

---
+++ api/src/mocks/user.mock.ts
@@ -0,0 +1,20 @@
+import type {User} from '../types/user.type.ts'; 
+
+const users = User[] = [{
+    id: 1,
+    name: 'Alex Bessa',
+    email: 'alexbessa@gmail.com',
+    password: 'jd@123'
+}, {
+    id: 2,
+    name: 'Aline Menezes'
+    email: 'alinemenezes@gmail.com',
+    senha: 'h372#78***'
+}, {
+    id: 3,
+    name: 'Ana'
+    email: 'ana@email.com',
+    password: 'hj@12548'
+}];
+
+export default users;
